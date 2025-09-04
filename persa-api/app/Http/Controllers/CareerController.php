4cb50096318c26a6442b892cf95e9b48e1da6a3d
danna marulanda <marulandadanna2@gmail.com>
2025-09-04 10:44:48 -0500

    Corrección Controllers Pt2
    Se creo y modifico CareerController.

--- persa-api/app/Http/Controllers/CareerController.php
+++ persa-api/app/Http/Controllers/CareerController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Career;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CareerController extends Controller
 {
+    private $rules = [
+        'name' => 'required|string|max:80',
+        'type' => 'required|string|max:255',
+       
+    ];
+
+    private $traductionAttributes = [
+        'name' => 'nombre',
+        'type' => 'tipo',
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $careers = Career::all();
+        return response()->json($careers, Response::HTTP_OK);
     }
 
     /**
@@ -19,30 +33,59 @@ class CareerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if (!empty($data)) {
+            return $data;
+        }
+
+        $career = Career::create($request->all());
+        $response = [
+            'message' => 'Registro creado exitosamente',
+            'career' => $career
+        ];
+
+        return response()->json($response, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Career $career)
     {
-        //
+        return response()->json($career, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Career $career)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if (!empty($data)) {
+            return $data;
+        }
+
+        $career->update($request->all());
+        $response = [
+            'message' => 'Registro actualizado exitosamente',
+            'career' => $career
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Career $career)
     {
-        //
+        $career -> delete();
+
+        $response = [
+            'message' => 'Registro eliminado exitosamente',
+            'career' => $career
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
     }
 }
