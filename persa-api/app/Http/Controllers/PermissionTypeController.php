a8798690427e7550fbb983878a4008440d93d9ca
Lina Vanessa S <lvb171@hotmail.com>
2025-09-04 12:26:55 -0500

    Controllers Pt6
    Se hizo, permissionType, Roles y se arreglo Permission

--- persa-api/app/Http/Controllers/PermissionTypeController.php
+++ persa-api/app/Http/Controllers/PermissionTypeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PermissionType;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PermissionTypeController extends Controller
 {
+    private $rules = [
+        'name' => 'required|string|max:255',
+    ];
+
+    private $traductionAttributes = [
+        'name' => 'nombre',
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $permission_types = PermissionType::all();
+        return response()->json($permission_types, Response::HTTP_OK);
     }
 
     /**
@@ -19,30 +30,58 @@ class PermissionTypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if (!empty($data)) {
+            return $data;
+        }
+
+        $permission_type = PermissionType::create($request->all());
+        $response = [
+            'message' => 'Tipo de permiso creado exitosamente',
+            'permissionType' => $permission_type
+        ];
+
+        return response()->json($response, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(PermissionType $permission_type)
     {
-        //
+        return response()->json($permission_type, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, PermissionType $permission_type)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if(!empty($data)){
+            return $data;
+        }
+
+        $permission_type->update($request->all());
+        $response = [
+            'message' => 'Tipo de permiso actualizado exitosamente',
+            'permissionType' => $permission_type
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(PermissionType $permission_type)
     {
-        //
+        $permission_type->delete();
+        $response = [
+            'message' => 'Tipo de permiso eliminado exitosamente',
+            'permissionType' => $permission_type
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
     }
 }
